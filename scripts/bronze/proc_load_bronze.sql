e7022cfb999d2bf1eea0a1e7914c8d22ff8b8437
Darrow75 <darrow75@live.com>
2026-09-19 07:50:56 -0400

    Fix stored procedure name in proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,6 +1,6 @@
 /*
 ===========================================================================
-Stored Procedure: Load Bronze Bronze Layer (Source -> Bronze)
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
 ===========================================================================
 Script Purpose:
 	This stored procedure loads date into the 'bronze' schema from external
