c97180f5f5fce2a90254f11f12378586c0287323
Darrow75 <darrow75@live.com>
2026-09-19 15:29:48 -0400

    Add quality checks script for Gold Layer validation
    This script performs quality checks on the Gold Layer, ensuring data integrity, uniqueness of keys, and referential integrity between fact and dimension tables.

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,96 @@
+/*
+==============================================================================
+Quality checks
+==============================================================================
+Script Purpose:
+	This script performs quality checks to validate the integrity, consistency,
+	and accuracy of the Gold Layer. These checks ensure:
+	- Uniqueness of surrogate keys in dimension tables.
+	- Referential integrity between fact and dimension tables.
+	- Validations of relationships in the data model for analytical purposes.
+
+Usage Notes:
+	- Runs these checks after data loading Silver Layer.
+	- Investigate and resolve any discrepancies found during the checks.
+==============================================================================
+*/
+
+-- ===========================================================================
+-- Checking 'gold.dim_customers'
+-- ===========================================================================
+
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No Results
+
+SELECT cst_id, COUNT(*) FROM
+	(SELECT
+		ci.cst_id,
+		ci.cst_key,
+		ci.cst_firstname,
+		ci.cst_lastname,
+		ci.cst_marital_status,
+		ci.cst_gndr,
+		ci.cst_create_date,
+		ca.gen,
+		la.cntry
+	FROM silver.crm_cust_info ci
+	LEFT JOIN silver.erp_cust_aZ12 ca
+	ON		  ci.cst_key = ca.cid
+	LEFT JOIN silver.erp_loc_a101 la
+	ON		  ci.cst_key = la.cid
+)t GROUP BY cst_id
+HAVING COUNT(*) > 1;
+
+-- Data Integration Check - Checks the integration of gender columns between
+-- 'silver.crm_cust_info' and 'silver.erp_cust_aZ12'
+
+SELECT DISTINCT
+	ci.cst_gndr,
+	ca.gen,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for Gender Info
+		 ELSE COALESCE(ca.gen, 'n/a')
+	END AS new_gen
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_aZ12 ca
+ON		  ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON		  ci.cst_key = la.cid;
+
+-- ===========================================================================
+-- Checking 'gold.product_key'
+-- ===========================================================================
+-- Check for Uniqueness of Product Key in gold.dim_customers
+-- Expectation: No Results
+
+SELECT prd_key, COUNT(*) FROM (
+SELECT
+	pn.prd_id,
+	pn.cat_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt,
+	pn.prd_end_dt,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL --Filter out all historical data
+)t GROUP BY prd_key
+HAVING COUNT(*) > 1;
+
+-- ===========================================================================
+-- Checking 'gold.fact_sales'
+-- ===========================================================================
+-- Check the data model connectivity between fact and dimensions.
+
+SELECT *
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE c.customer_key IS NULL;
